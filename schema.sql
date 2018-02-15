7015d0e433b63b503d30c3607956cf0aad861690
miriam30 <miriam.alvarez.p96@gmail.com>
2018-02-15 11:09:15 -0600

    get request fixxed

--- schema.sql
+++ schema.sql
@@ -5,9 +5,11 @@ CREATE DATABASE recipes;
 USE recipes;
 
 CREATE TABLE list (
-  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  url varchar NOT NULL,
-  description varchar(50) NOT NULL
+  id int NOT NULL AUTO_INCREMENT,
+  url varchar(255) NOT NULL,
+  description varchar(50) NOT NULL,
+  PRIMARY KEY (ID)
 );
+
 INSERT INTO list (url, description) VALUES ('https://www.jamieoliver.com/recipes/vegetables-recipes/ratatouille/', 'ratatouille');
 INSERT INTO list (url, description) VALUES ('https://www.vegansociety.com/resources/recipes/quick-easy/tofu-scramble', 'tofu scramble');
